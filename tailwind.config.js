ccc1bc2a307e1b18d2bc472d16b0e1e7e2337d96
Harsh Chandwani <harshchandwani50028@gmail.com>
2023-06-11 09:33:39 +0530

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    screens: {
+      md: { max: "767px" },
+    },
+    fontFamily: {
+      raleway: ["Raleway", "sans-serif"],
+    },
+    colors: {
+      bc: "#FEFEFE",
+      primary: "#1C1C1C",
+      secondary: "#DD8B6B",
+      danger: "#81978B",
+      lightBlue: "#A0BEE6",
     },
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-}
+};
